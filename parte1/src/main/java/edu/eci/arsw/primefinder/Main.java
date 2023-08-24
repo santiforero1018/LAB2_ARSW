a22b5c221d90bfe939ce12eab8e12c2bffaf7d58
unknown <santiago.forero-y@labinfo.is.escuelaing.edu.co>
2023-08-24 10:00:57 -0500

    seguir mirando parte 3

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,12 +1,17 @@
 package edu.eci.arsw.primefinder;
 
+
+
 public class Main {
 
 	public static void main(String[] args) {
+		PrimeFinderThread pft1=new PrimeFinderThread("hilo 1",0, 9999999);
+		PrimeFinderThread pft2=new PrimeFinderThread("hilo 2",10000000, 19999999);
+		PrimeFinderThread pft3=new PrimeFinderThread("hilo 3",20000000, 30000000);
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
+		pft1.start();
+		pft2.start();
+		pft3.start();
 		
 		
 	}
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -1,5 +1,6 @@
 package edu.eci.arsw.primefinder;
 
+import java.time.Duration;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,29 +11,35 @@ public class PrimeFinderThread extends Thread{
 	
 	private List<Integer> primes=new LinkedList<Integer>();
 	
-	public PrimeFinderThread(int a, int b) {
-		super();
+	private Long startTime, endTime;
+
+	public PrimeFinderThread(String name, int a, int b) {
+		super(name);
 		this.a = a;
 		this.b = b;
 	}
 
+	@Override
 	public void run(){
+		this.startTime = System.currentTimeMillis();
 		for (int i=a;i<=b;i++){						
 			if (isPrime(i)){
 				primes.add(i);
-				System.out.println(i);
+				System.out.println(this.getName() + ": " + i);
 			}
 		}
 		
 		
 	}
 	
-	boolean isPrime(int n) {
+	private synchronized boolean isPrime(int n) {
 	    if (n%2==0) return false;
 	    for(int i=3;i*i<=n;i+=2) {
 	        if(n%i==0)
 	            return false;
+			
 	    }
+		this.endTime = System.currentTimeMillis();
 	    return true;
 	}
 
@@ -40,7 +47,14 @@ public class PrimeFinderThread extends Thread{
 		return primes;
 	}
 	
+	private void pause() throws InterruptedException{
+		Long start = System.currentTimeMillis();
+
+
+	}
 	
-	
+	private void running(){
+
+	}
 	
 }
