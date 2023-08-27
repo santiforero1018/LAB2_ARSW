6cf653da51d1d07aecf50587ec084572ce1d8234
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-08-26 19:49:51 -0500

    avance punto 3 parte 1 importante

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -7,26 +7,26 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		PrimeFinderThread pft1=new PrimeFinderThread("hilo 1",0, 9999999);
-		PrimeFinderThread pft2=new PrimeFinderThread("hilo 2",10000000, 19999999);
-		PrimeFinderThread pft3=new PrimeFinderThread("hilo 3",20000000, 30000000);
+		PrimeFinderThread pft1 = new PrimeFinderThread("hilo 1", 0, 9999999);
+		PrimeFinderThread pft2 = new PrimeFinderThread("hilo 2", 10000000, 19999999);
+		PrimeFinderThread pft3 = new PrimeFinderThread("hilo 3", 20000000, 30000000);
 
 		pft1.start();
 		pft2.start();
 		pft3.start();
 
-		
-		System.out.println("Execution paused. Press ENTER to resume...");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		br.readLine();
+		while (pft1.isAlive() || pft2.isAlive() || pft3.isAlive()) {
 
-		pft1.running();
-		pft2.running();
-		pft3.running();
-		
+			System.out.println("Execution paused. Press ENTER to resume...");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			br.readLine();
 
-        
+			pft1.running();
+			pft2.running();
+			pft3.running();
+
+		}
 
 	}
-	
+
 }
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,12 +3,11 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
+public class PrimeFinderThread extends Thread {
 
-	
-	int a,b;
-	
-	private List<Integer> primes=new LinkedList<Integer>();
+	int a, b;
+
+	private List<Integer> primes = new LinkedList<Integer>();
 	private Object lock = new Object();
 	private Long startTime;
 	private boolean paused = false;
@@ -20,60 +19,66 @@ public class PrimeFinderThread extends Thread{
 	}
 
 	@Override
-    public void run() {
-        while (true) {
-            this.startTime = System.currentTimeMillis();
-            long targetPauseTime = startTime + 5000;
-            synchronized (lock) {
-				for(int i = a; i<=b && (System.currentTimeMillis() < targetPauseTime) && !paused;i++){
+	public void run() {
+		if (a > b){
+			return;
+		}
+		while (true) {
+			this.startTime = System.currentTimeMillis();
+			long targetPauseTime = startTime + 5000;
+			synchronized (lock) {
+				for (int i = a; i <= b && (System.currentTimeMillis() < targetPauseTime) && !paused; i++) {
 					if (isPrime(i)) {
-                        primes.add(i);
-						//System.out.println(this.getName()+":"+i);
-                    }
-					
+						primes.add(i);
+						// System.out.println(this.getName()+":"+i);
+					}
+
+					a = i;
 				}
 
-                if (System.currentTimeMillis() >= targetPauseTime) {
-                    try {
-						//System.out.println("Wait");
+				if (System.currentTimeMillis() >= targetPauseTime) {
+					try {
 						this.pause();
-                        lock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
-	
-	private synchronized boolean isPrime(int n) {
-	    if (n%2==0) return false;
-	    for(int i=3;i*i<=n;i+=2) {
-	        if(n%i==0)
-	            return false;
-			
-	    }
-	    return true;
+						System.out.println(this.getName() + ": " + this.getPrimes().size() + "current aa: " +a);
+						lock.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
+	}
+
+	private boolean isPrime(int n) {
+		if (n % 2 == 0)
+			return false;
+		for (int i = 3; i * i <= n; i += 2) {
+			if (n % i == 0)
+				return false;
+
+		}
+		return true;
 	}
 
 	public List<Integer> getPrimes() {
 		return primes;
 	}
-	
-	public synchronized void pause() throws InterruptedException{
-			paused = true;
+
+	public void pause() {
+		paused = true;
 
 	}
-	
-	public synchronized void running(){
-			synchronized (lock) {
-				paused = false;
-				lock.notify();
-			}
+
+	public synchronized void running() {
+		synchronized (lock) {
+			paused = false;
+			lock.notify();
+		}
 	}
 
-	public boolean isPaused(){
+	public boolean isPaused() {
 		return paused;
 	}
-	
+
 }
