99b218edb5d7201a1f1d0b5c2c770ac306a28c5e
Santiago Forero <santiago.forero-y@mail.escuelaing.edu.co>
2023-08-28 21:33:54 -0500

    avance punto 2 parte 2

--- parte2/src/main/java/arsw/threads/Galgo.java
+++ parte2/src/main/java/arsw/threads/Galgo.java
@@ -15,30 +15,29 @@ public class Galgo extends Thread {
 		super(name);
 		this.carril = carril;
 		paso = 0;
-		this.regl=reg;
+		this.regl = reg;
 	}
 
-	public void corra() throws InterruptedException {
-		while (paso < carril.size()) {			
+	public synchronized void corra() throws InterruptedException {
+		while (paso < carril.size()) {
 			Thread.sleep(100);
 			carril.setPasoOn(paso++);
 			carril.displayPasos(paso);
-			
-			if (paso == carril.size()) {						
+
+			if (paso == carril.size()) {
 				carril.finish();
-				int ubicacion=regl.getUltimaPosicionAlcanzada();
-				regl.setUltimaPosicionAlcanzada(ubicacion+1);
-				System.out.println("El galgo "+this.getName()+" llego en la posicion "+ubicacion);
-				if (ubicacion==1){
+				int ubicacion = regl.getUltimaPosicionAlcanzada();
+				regl.setUltimaPosicionAlcanzada(ubicacion + 1);
+				System.out.println("El galgo " + this.getName() + " llego en la posicion " + ubicacion);
+				if (ubicacion == 1) {
 					regl.setGanador(this.getName());
 				}
 			}
 		}
 	}
 
-
 	@Override
-	public void run(){
+	public void run() {
 		try {
 			corra();
 		} catch (InterruptedException e) {
